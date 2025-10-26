628945b1daf4e803b3bd9ab70a33041c2aec68fe
leosarturi <sarturilazier@gmail.com>
2025-10-26 17:20:38 -0300

    fix movement

--- SoccerServer/Program.cs
+++ SoccerServer/Program.cs
@@ -48,9 +48,9 @@ class Match
     private const float groundY = -5f;
     private const float playerRadius = 1f;
     private const float ballRadius = 0.5f;
-    private const float jumpHeight = 50f;
-    private const float kickPower = 100f;
-    private const float playerSpeed = 30f;
+    private const float jumpHeight = 500f;
+    private const float kickPower = 1000f;
+    private const float playerSpeed = 100f;
 
     private float halfWidth = fieldWidth / 2f;
     private float halfHeight = fieldHeight / 2f;
